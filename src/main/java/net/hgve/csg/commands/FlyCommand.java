34efe773d853d5f703b4a74e4b5f2293f9ef7242
GrijsjePlay <113257202+GrijsjePlay@users.noreply.github.com>
2024-09-15 21:54:38 +0200

    finished /fly

--- src/main/java/net/hgve/csg/commands/FlyCommand.java
+++ src/main/java/net/hgve/csg/commands/FlyCommand.java
@@ -1,6 +1,8 @@
 package net.hgve.csg.commands;
 
 import net.hgve.csg.CSG;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,6 +27,48 @@ public class FlyCommand implements CommandExecutor {
             return true;
         }
 
+        if (!(sender.hasPermission("CSG.fly"))) {
+            sender.sendMessage(plugin.getMessageManager().getMessage("permission_error"));
+            return true;
+        }
+
+        if (args.length > 1) {
+            sender.sendMessage(plugin.getMessageManager().getMessage("invalid_args"));
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            if (player.getAllowFlight()) {
+                player.setAllowFlight(false);
+                player.sendMessage(ChatColor.AQUA + "Fly disabled");
+            } else {
+                player.setAllowFlight(true);
+                player.sendMessage(ChatColor.AQUA + "Fly enabled");
+            }
+            return true;
+        }
+
+        if (args.length == 1) {
+            Player target = Bukkit.getServer().getPlayerExact(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(plugin.getMessageManager().getMessage(""));
+                return true;
+            }
+
+            if (target.getAllowFlight()) {
+                target.setAllowFlight(false);
+                sender.sendMessage("Enabled flying for " + target.getName());
+            } else {
+                target.setAllowFlight(true);
+                sender.sendMessage("Disabled flying for " + target.getName());
+            }
+            return true;
+        }
+
+        sender.sendMessage(plugin.getMessageManager().getMessage("invalid_args"));
         return true;
     }
 }
